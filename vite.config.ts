1aa89d3310cb46c2796d2f09f7c505e7ad8e60e9
luiz-maranhao <luizfmaranhao2003@gmail.com>
2026-01-21 15:23:07 -0300

    fix: adiciona base path para o github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Adicione esta linha com o nome EXATO do seu repositório no GitHub
-  base: '/NOME_DO_SEU_REPOSITORIO/', 
+  // Adicione esta linha exatamente assim:
+  base: '/book-catalog/', 
 })
